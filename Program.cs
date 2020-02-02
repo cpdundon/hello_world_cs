006c7e83770e29e6c7308c5e1bfcbd0b2056855e
Christopher P. Dundon <chris.dundon@gmail.com>
2020-02-01 23:46:36 -0500

    baby steps... working to get C# up and running.  Lots of config education

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DotnetBot {
+	public static class Program {
+		
+		public static void Main(string[] args) {
+
+			string message = "";
+			if (args.length < 1) {
+				message = "Welcome to .NET Core!"
+			}
+			else {
+				foreach (string item in args) {
+					message += item;
+				}
+			}
+		}
+	}
+}
